d2e6253cfb6440378e5cb1f7b65a9ba786bc388b
Kloberdanz <Klobermdq@gmail.com>
2021-10-21 08:42:35 -0300

    Primer commit

---
+++ src/js/componentes.js
@@ -0,0 +1,17 @@
+import '../css/componentes.css';
+import  webpackLogo from '../assets/img/webpack-logo.png'
+
+export const saludar = ( nombre ) => {
+
+    console.log(`creando etiqueta h1`);
+
+    const h1 = document.createElement(`h1`);
+    h1.innerText = `hola, ${ nombre } `;
+    document.body.append( h1 );
+
+    //img
+    const img = document.createElement ('img');
+    img.src = webpackLogo;
+    document.body.append ( img );
+
+}
